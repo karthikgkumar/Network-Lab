708713943f3b6952e8c377bde9a44c20921611de
Karthik G Kumar <93706007+karthikgkumar@users.noreply.github.com>
2024-07-09 00:42:27 +0530

    Update server.c

--- cycle-4/UDP/c/server.c
+++ cycle-4/UDP/c/server.c
@@ -1,75 +1,55 @@
-// server.c
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <time.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<unistd.h>
 
-#define PORT 8888
-#define BUFFER_SIZE 1024
+int sock_fd=0;
+struct sockaddr_in server_addr, client_addr;
 
-void handle_client(int sockfd, struct sockaddr_in *client_addr, socklen_t addr_len) {
-    char buffer[BUFFER_SIZE];
-    time_t current_time;
-    struct tm *time_info;
-    char time_string[BUFFER_SIZE];
 
-    // Get current time
-    time(&current_time);
-    time_info = localtime(&current_time);
-
-    // Format time as a string
-    strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", time_info);
-
-    // Send time to client
-    sendto(sockfd, time_string, strlen(time_string), 0, 
-           (struct sockaddr*)client_addr, addr_len);
-}
-
-int main() {
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE];
-
-    // Create UDP socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    memset(&client_addr, 0, sizeof(client_addr));
-
-    // Configure server address
-    server_addr.sin_family = AF_INET;
+void main(){
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Bind socket to address and port
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_family = AF_INET;
+    
+    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
-        exit(EXIT_FAILURE);
+        close(sock_fd);
+        return;
     }
 
-    printf("UDP Time Server listening on port %d...\n", PORT);
+    
+    printf("Client connected\n");
+    
+    while(1){
+        char r_buff[100]="";
+        char s_buff[100]="";
 
-    while (1) {
-        // Receive request from client
-        int len = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, 
-                           (struct sockaddr *)&client_addr, &addr_len);
-        buffer[len] = '\0';
-
-        printf("Received request from %s:%d\n", 
-               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-        // Handle client request
-        handle_client(sockfd, &client_addr, addr_len);
+        socklen_t n, client_addr_size;
+        client_addr_size=sizeof(client_addr);
+        int bytes_received = recvfrom(sock_fd, r_buff, sizeof(r_buff) - 1, 0,(struct sockaddr *)&client_addr, &client_addr_size );
+        if (bytes_received > 0) {
+            r_buff[bytes_received] = '\0';
+            printf("Received: %s\n", r_buff);
+            
+            printf("Reply: ");
+            fgets(s_buff, sizeof(s_buff), stdin);
+            s_buff[strcspn(s_buff, "\n")] = 0;  // Remove newline
+            
+            sendto(sock_fd, s_buff, strlen(s_buff), 0, (struct sockaddr *)&client_addr, client_addr_size);
+        }
+        
+        if (strcmp(r_buff, "end") == 0) {
+            break;
+        }
     }
-
-    close(sockfd);
-    return 0;
+    
+    close(sock_fd);
+    printf("Server stopped\n");
 }
+
